640b8b6f21721cf0094841fcb5071db6649b32d8
Curso C# <ce.villegas92@gmail.com>
2026-03-18 17:46:23 -0600

    Detallitos
    nada importante

--- MiPrimeraAPI/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ MiPrimeraAPI/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cacd948e744ff2414368cd65bfa8f77f9aa1277")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08c0358d2ead522835f11506fcb7b606ee8128f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
